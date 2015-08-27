fac8faca843035f592145299efe3e102cf621027
brijrajgohil <brijrajsinhgohil@yahoo.com>
2015-08-27 18:16:27 +0530

    Initial Commit
    Experimenting with SpriteKit.

---
+++ IK-Ninja/GameScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameScene.swift
+//  Ninja-Punch
+//
+//  Created by Beetu on 27/8/2015.
+//  Copyright (c) 2015 Brijrajsinh Gohil. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+  
+  override func didMoveToView(view: SKView) {
+   
+  }
+}
