874064561889e3a47892cc853faabaf32cec41e4
brijrajgohil <brijrajsinhgohil@yahoo.com>
2015-08-28 12:41:33 +0530

    Inverse Kinematics

--- IK-Ninja/GameScene.swift
+++ IK-Ninja/GameScene.swift
@@ -9,8 +9,41 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var shadow: SKNode!
+    var lowerTorso: SKNode!
+    var upperTorso: SKNode!
+    var upperArmFront: SKNode!
+    var lowerArmFront: SKNode!
+    var fistFront: SKNode!
   
   override func didMoveToView(view: SKView) {
+    upperTorso = lowerTorso.childNodeWithName("torso_upper")
+    upperArmFront = upperTorso.childNodeWithName("arm_upper_front")
+    lowerArmFront = upperArmFront.childNodeWithName("arm_lower_front")
+    fistFront = lowerArmFront.childNodeWithName("fist_front")
+
+    lowerTorso = childNodeWithName("torso_lower")
+    lowerTorso.position = CGPoint(x: CGRectGetMidX(frame), y: CGRectGetMidY(frame) - 30)
+    
+    shadow = childNodeWithName("shadow")
+    shadow.position = CGPoint(x: CGRectGetMidX(frame), y: CGRectGetMidY(frame) - 100)
    
   }
+    
+    
+    
+    func punchAtLocation(location: CGPoint) {
+        // 1
+        let punch = SKAction.reachTo(location, rootNode: upperArmFront, duration: 0.1)
+        // 2
+        fistFront.runAction(punch)
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        for touch: AnyObject in touches {
+            let location = touch.locationInNode(self)
+            punchAtLocation(location)
+        }
+    }
 }
